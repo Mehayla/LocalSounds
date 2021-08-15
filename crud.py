0b91e5af9c0b9312244b1fa0f9c8512b8d1535ee
Bella and Mehayla <mehaylac@gmail.com>
2021-08-15 16:39:57 +0000

    And, the day is done

---
+++ crud.py
@@ -0,0 +1,12 @@
+# import model
+
+# 1. Check if relationships in model.py is okay and ask about testing data
+# 2. Access the spotify API/JSON - use curl and get it in a JSON to play with
+# 3. Figure parse through the information give back whats needed via query
+
+# def populate_locations():
+#     """ Something that will take the csv read it and populae my db """
+
+# def make_new_user():
+#     """ Makes a new instance of the class user"""
+
