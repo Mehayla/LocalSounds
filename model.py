9f14917042713326cdc06474b20aa3e8a1530b31
Bella and Mehayla <mehaylac@gmail.com>
2021-08-15 16:18:22 +0000

    New files and db connection. Whoot!

--- model.py
+++ model.py
@@ -33,7 +33,7 @@ class Artist(db.Model):
                         primary_key = True,
                         autoincrement = True)
     artist_name = db.Column(db.String)
-    artist_URI = db.Column(db.URL, unique = True) #Is URL even a column input type? Double chek this
+    artist_URI = db.Column(db.String, unique = True) #Is URL even a column input type? Double chek this
     location_id = db.Column(db.Integer, db.ForeignKey('Locations.location_id'))
 
     location = db.relationship("Locations", backref = "artist")
@@ -57,3 +57,30 @@ class Locations(db.Model):
     def __repr__(self):
         return f"Location {self.city}, {self.state} has been created."
 
+
+# def connect_to_db(flask_app, db_uri = '????', echo = True):
+#     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+#     flask_app.config["SQLALCHEMY_ECHO"] = echo
+#     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#     db.app = flask_app
+#     db.init_app(flask_app)
+
+#     print("Connected to the Data Base")
+
+
+# if __name__ == "__main__":
+#     from server import app
+#     connect_to_db(app)
+
+#     test_user = User(u_name = 'Tesy', u_password = 'Besty')
+#     db.session.add(test_user)
+#     db.session.commit()
+
+#     test_artist = Artist(artist_name = 'Potato', artist_URI = 'http://TheSkinsss.com' )
+#     db.session.add(test_artist)
+#     db.session.commit()
+
+#     test_location = Locations(state = 'California', city = 'San Francisco', zipcode = 94703)
+#     db.session.add(test_location)
+#     db.session.commit()
