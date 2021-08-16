09adf5f33310949b35c56cdc924979cef9a12272
Bella and Mehayla <mehaylac@gmail.com>
2021-08-16 13:22:05 +0000

    Syntax is hard. . .

--- server.py
+++ server.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 ## INSERT ROUTES HERE ##
 
 
-if __name__ = '__main__':
+if __name__ == "__main__":
     #DebugToolbarExtension(app)
     app.run(host = "0.0.0.0", debug = True)
