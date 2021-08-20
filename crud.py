b59a2213ebe39540db44b010708aa45e03f4684e
Bella and Mehayla <mehaylac@gmail.com>
2021-08-20 14:28:38 +0000

    Finshed for today

--- crud.py
+++ crud.py
@@ -14,15 +14,21 @@ from model import db, User, Artist, Location, connect_to_db
 def create_user(name, password, city, state):
     """ Adds a new user to the user table"""
 
+    city = city.lower()
+    state = state.lower()
+
     user = User.query.filter(User.u_name == name).one_or_none() #us.one_or_none() - returns an error when nothing 
     location = Location.query.filter(Location.city == city, state == state).one_or_none()
 
     if user == None:  
         if location == None:  #Checks if location doesn't exist
-            state_id = Location.query.filter_by(state = state, city = None) #If location doesn't exist yet, give them a state code ALSO ADD A FLASH? ALSO UPDATE WHEN AN ARTIST ADDS A NEW LOCATION?
-            new_user = User(u_name = name, u_password = password, location_id = state_id.location_id) # Is this still a FK???
+            state = Location.query.filter_by(state = state, city = None).one() #If location doesn't exist yet, give them a state code ALSO ADD A FLASH? ALSO UPDATE WHEN AN ARTIST ADDS A NEW LOCATION?
+            state_id = state.location_id
+
+            new_user = User(u_name = name, u_password = password, location_id = state_id) # Is this still a FK???
             db.session.add(new_user)
             db.session.commit()
+            return new_user
         else:
             loc_obj = Location.query.filter_by(city = city, state = state).one() # gets the PK from location db 
             new_user = User(u_name = name, u_password = password, location_id = loc_obj.location_id) # Is this still a FK???
@@ -37,12 +43,15 @@ def create_user(name, password, city, state):
 def create_artist(artist_name, artist_URI, city, state):
     """ Adds a new artist to the artist table"""
 
+    city = city.lower()
+    state = state.lower()
+
     # current_artist = db.session.query(User.u_name).all()
     artist = User.query.filter(Artist.artist_name == artist_name).one_or_none()
-    location = User.query.filter(Location.city == city, state == state).one_or_none()
+    location = Location.query.filter(Location.city == city, state == state).one_or_none()
 
     if artist == None:
-        if location == None:        # Creates a new artist and a new location OR SHOULD I call the other function? #SCALABILITY
+        if location == None:        # See create_location for why this is here
             # new_location = Location(city = city, state = state)
             # db.session.add(new_location)
             # db.session.commit()
@@ -63,20 +72,24 @@ def create_artist(artist_name, artist_URI, city, state):
        
 
 
-def create_location(state, city):
+def create_location(city, state):
     """ Adds a new location to the DB"""
 
     # This should be able to take in a location 2 ways.
-    # 1. When the csv is loaded into the application via seed
-    # 2. When a user or artist inputs a location that is not currently in the db
+    # 1. When the csv is loaded into the application via seed 
+    # 2. When a user or artist inputs a location that is not currently in the db 3.0
 
     state = state.lower()
     city = city.lower()
+    location = Location.query.filter(Location.city == city, state == state).one_or_none()
 
-    new_location = Location(state = state, city = city)
-    db.session.add(new_location)
-    db.session.commit()
-    return new_location 
+    if location == None:
+        new_location = Location(state = state, city = city)
+        db.session.add(new_location)
+        db.session.commit()
+        return new_location
+    else:
+        pass
 
 
 
--- model.py
+++ model.py
@@ -88,6 +88,10 @@ if __name__ == "__main__":
     db.session.add(test_artist)
     db.session.commit()
 
-    test_location = Location(state = 'California', city = 'San Francisco')
+    test_location = Location(state = 'california', city = 'san francisco')
     db.session.add(test_location)
+    db.session.commit()
+
+    test_state_id = Location(state = 'ohio')
+    db.session.add(test_state_id)
     db.session.commit()
