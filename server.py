160c334f48c2d0e864e500b15f800bce8a1319e5
Bella and Mehayla <mehaylac@gmail.com>
2021-08-26 14:19:55 +0000

    Moving and groovin. Here are some files

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """ Server for LocalSound app """
 
-from flask import Flask
+from flask import Flask, render_template, request
 import crud
 
 app = Flask(__name__)
@@ -9,37 +9,40 @@ app = Flask(__name__)
 ## INSERT ROUTES HERE ##
 # JSON zipcode/city - 
 
-# @app.route('/sign-up')
-# def user_type():
-#     if user == person:
-#         make_new_user()
-#     else:
-#         make_new_artist()
-#         Or redirects?????
+
+@app.route('/')
+def welcome_home():
+    """ Landing page """
+
+    return render_template("home.html")
+
+@app.route('/search')
+def find_scene():
+    """ Search for artists based on locations"""
 
 
-# @app.route('/')
-# def find_scene():
-#     """ Landing page """
-
-
-# @app.route('/search')
-# def find_scene():
-#     """ Search for artists based on locations"""
-
-
-# @app.route('/playlist')
-# def find_scene():
-#     """ Search for artists based on locations"""
+@app.route('/playlist')
+def heres_loc_artist():
+    """ Search for artists based on locations"""
 
 # Where the API recommendation is
 
 
-# @app.route('/artistinfo')
-# def find_scene():
-#     """ Search for artists based on locations"""
+@app.route('/artistinfo')
+def more_on_the_band():
+    """ Search for artists based on locations"""
 # API for artist information
 # Bandcamp link etc
+    return render_template("artistinfo")
+
+
+@app.route('/sign-up')
+def get_dat_user():
+    if user == person:
+        make_new_user()
+    else:
+        make_new_artist()
+        Or redirects?????
 
 
 if __name__ == "__main__":
