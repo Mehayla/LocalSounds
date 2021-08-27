67c42afbc482028ec0ff207e5091fc4a9c52b3f4
Bella and Mehayla <mehaylac@gmail.com>
2021-08-27 11:14:53 +0000

    Ugghhhhhh

--- server.py
+++ server.py
@@ -4,29 +4,39 @@ from model import connect_to_db
 from flask import Flask, render_template, request, jsonify
 import crud
 import json
-import requests
 
 app = Flask(__name__)
+app.secret_key = 'PoTaToS'
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+API_KEY = os.environ['8f49c9011a8b4b078b1e282a748d0cf3']
 
 # Is this the right place????? #
-# music_json = open('music.json').read()
-# music_dic = json.loads(music_json)
-# print(music_dic)
-# GET https://api.spotify.com/v1/tracks
-# Try out a GET request in here
+# When would I use get_artists?
 
 @app.route('/')
 def welcome_home():
     """ Landing page """
 
-    return render_template("home.html")
+    # tracks = request.arg.get('tracks','')
+
+    url = 'https://api.spotify.com/v1/tracks'
+    payload = {'apikey', API_KEY}
+    tracks = res.json()
+
+    playlist = []
+
+    return render_template("home.html", results=playlist)
 
 
 @app.route('/playlist')
 def heres_loc_artist():
     """ Search for artists based on locations"""
+    music_json = open('music.json').read()
+    music_dic = json.loads(music_json)
+    print(music_dic)
 
 # Where the API recommendation is
+
 
 
 @app.route('/artistinfo')
