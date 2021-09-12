8309dc90f5a1da658b30b9d7a95f7f766f8c614e
Bella and Mehayla <mehaylac@gmail.com>
2021-09-12 12:43:54 +0000

    got some style

--- crud.py
+++ crud.py
@@ -187,7 +187,7 @@ def get_artists(city, state):
     for artist in artists:
         rec_list.append(artist.artist_name)
 
-    rec_lis = random.sample(rec_list, k=10) #Picks 50 random artists
+    rec_lis = random.sample(rec_list, k=10) #Picks 50 random artists #Maybe change spotify_info to break at 10?
     return rec_lis
 
 
